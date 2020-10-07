e0ad3cd96ff6e16726af485f95c0133f4f0033c3
naunow <naunow00@gmail.com>
2020-10-07 22:18:02 +0900

    - added a feature for changing text color.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,32 +2,78 @@ import 'package:flutter/material.dart';
 import 'package:neonsignboard/screens/neon_display.dart';
 import 'package:neonsignboard/size_config.dart';
 
+// class Home extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     SizeConfig().init(context);
+//     return Scaffold(
+//       appBar: AppBar(
+//         actions: <Widget>[
+//           IconButton(
+//               icon: Icon(Icons.add),
+//               onPressed: () => Navigator.push(
+//                   context,
+//                   MaterialPageRoute(
+//                     builder: (context) => NeonDisplay(),
+//                   )))
+//         ],
+//       ),
+//       resizeToAvoidBottomInset: false,
+//       body: OrientationBuilder(builder: (context, orientation) {
+//         return GridView.builder(
+//           itemBuilder: (context, index) {
+//             return Placeholder();
+//           },
+//           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+//               crossAxisCount: orientation == Orientation.landscape ? 3 : 2),
+//         );
+//       }),
+//     );
+//   }
+// }
+
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Scaffold(
-      appBar: AppBar(
-        actions: <Widget>[
-          IconButton(
-              icon: Icon(Icons.add),
-              onPressed: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => NeonDisplay(),
-                  )))
+      resizeToAvoidBottomInset: false,
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            pinned: false,
+            snap: false,
+            floating: false,
+            title: Text('App Bar'),
+            actions: <Widget>[
+              IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => NeonDisplay(),
+                      )))
+            ],
+          ),
+          SliverGrid(
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: SizeConfig.screenWidth / 2,
+              mainAxisSpacing: 10.0,
+              crossAxisSpacing: 10.0,
+              childAspectRatio: 3.0,
+            ),
+            delegate: SliverChildBuilderDelegate((context, index) {
+              return OrientationBuilder(builder: (context, orientation) {
+                return Container(
+                  alignment: Alignment.center,
+                  color: Colors.teal[100 * (index % 9)],
+                  child: Text('grid item $index'),
+                );
+              });
+            }, childCount: 50),
+          ),
         ],
       ),
-      resizeToAvoidBottomInset: false,
-      body: OrientationBuilder(builder: (context, orientation) {
-        return GridView.builder(
-          itemBuilder: (context, index) {
-            return Placeholder();
-          },
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: orientation == Orientation.landscape ? 3 : 2),
-        );
-      }),
     );
   }
 }
--- lib/screens/neon_display.dart
+++ lib/screens/neon_display.dart
@@ -16,33 +16,90 @@ class NeonDisplay extends StatelessWidget {
       resizeToAvoidBottomInset: false,
       body: OrientationBuilder(
         builder: (context, orientation) {
-          bool showMenu = false;
-          return InkWell(
-            onTap: () {
-              showMenu ? showMenu = false : showMenu = true;
-              debugPrint(showMenu.toString());
-            },
-            child: Container(
-                width: SizeConfig.screenWidth,
-                height: SizeConfig.screenHeight,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage(orientation == Orientation.landscape
-                          // https://www.beiz.jp/download_P/stone/00096.html
-                          ? backGroundHorizontalImage
-                          : backGroundVerticalImage),
-                      fit: BoxFit.fill),
-                ),
-                child: Center(
-                  child: NeonText(
-                    text: 'open',
-                    fontSize: 120,
-                    fontFamily: 'Neoneon',
-                    color: kOrangeColor,
-                  ),
-                )),
-          );
+          return Stack(children: [
+            Container(
+              width: SizeConfig.screenWidth,
+              height: SizeConfig.screenHeight,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage(orientation == Orientation.landscape
+                        // https://www.beiz.jp/download_P/stone/00096.html
+                        ? backGroundHorizontalImage
+                        : backGroundVerticalImage),
+                    fit: BoxFit.fill),
+              ),
+              child: ColorBar(),
+            ),
+          ]);
         },
+      ),
+    );
+  }
+}
+
+class ColorBar extends StatefulWidget {
+  @override
+  _ColorBarState createState() => _ColorBarState();
+}
+
+class _ColorBarState extends State<ColorBar> {
+  int selectedColorIndex = 0;
+
+  final List<Color> colors = [
+    kOrangeColor,
+    kGreenColor,
+    kRedColor,
+    kBlueColor,
+    kYellowColor,
+    kPinkColor
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    // Top bar
+    return Container(
+      color: Colors.transparent,
+      height: SizeConfig.screenHeight,
+      width: SizeConfig.screenWidth,
+      child: Stack(
+        children: <Widget>[
+          ListView.builder(
+            itemCount: colors.length,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              // Color selector
+              return GestureDetector(
+                onTap: () {
+                  setState(() {
+                    selectedColorIndex = index;
+                  });
+                },
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                  child: Text(
+                    'Color',
+                    style: TextStyle(
+                        color: colors[index],
+                        fontSize: 30,
+                        fontWeight: index == selectedColorIndex
+                            ? FontWeight.bold
+                            : FontWeight.normal,
+                        fontFamily: 'Neoneon'),
+                  ),
+                ),
+              );
+            },
+          ),
+          Center(
+            child: NeonText(
+              text: 'open',
+              fontSize: 120,
+              fontFamily: 'Neoneon',
+              color: colors[selectedColorIndex],
+            ),
+          )
+        ],
       ),
     );
   }
